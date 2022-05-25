230e1736d43d85e816f4c0cb9a447ef007ce2354
Marva Athatillah <ahnafmarva@gmail.com>
2022-05-25 14:00:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/alarmapp/room/AlarmDao.kt
@@ -0,0 +1,18 @@
+package com.example.alarmapp.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface AlarmDao {
+    @Insert
+    fun addAlarm (alarm : Alarm)
+
+    @Query ("SELECT * FROM alarm")
+    // LiveData untuk memberitahu perubahan data yang terjadi di activity secara langsung
+    // sehingga recyclerview dapat memperbarui data secara langsung
+    fun getAlarm(): LiveData<List<Alarm>>
+
+    @Delete
+    fun deleteAlarm(alarm: Alarm)
+}
